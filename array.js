3d6d2948867800b5f16f0724d763fa3e9e3b5032
Generosa Litton <glcodeworks@gmail.com>
2024-04-28 18:07:40 -0500

    slice to remove item in an array

--- array.js
+++ array.js
@@ -2,4 +2,15 @@ function first(arr) {
   return arr[0];
 }
 
-console.log(first([]));
+// console.log(first([]));
+
+function last(arr) {
+  return arr[arr.length - 1];
+}
+
+// console.log(last(["Earth", "Moon", "Mars"]));
+
+let energy = ["fossil", "solar", "wind", "tidal", "fusion"];
+const newEnergy = energy.slice(1);
+newEnergy.push("geothermal");
+console.log(newEnergy);
